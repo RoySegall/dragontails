857a7bdc2c8620ff3db4aad3b19ef648934e086e
RoySegall <roy.segall@gizra.com>
2018-02-26 18:19:02 +0200

    commit

---
+++ src/Entity/Attendance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Social\Entity;
+
+use Social\EntityBase;
+
+class Attendance extends EntityBase {
+
+  protected $table = 'attendance';
+
+}
---
+++ src/Entity/EmployeeRoles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Social\Entity;
+
+use Social\EntityBase;
+
+class EmployeeRoles extends EntityBase {
+
+  protected $table = 'employee_roles';
+
+}
